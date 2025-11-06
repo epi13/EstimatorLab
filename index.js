34a00e93b4debc1613499071bc1329bb3d2aea83
epi13 <epicureousapc@gmail.com>
2025-11-06 11:12:41 -0900

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const navigationData = [
           { label: 'XLSX to LLM', href: 'src/miscellaneous/xlsx-to-llm/xlsx-to-llm.html', keywords: 'llm token saver' },
           { label: 'Scaler', href: 'src/miscellaneous/high-level-scaler/high-level-scaler.html', keywords: 'high level scaler' },
           { label: 'PDF Extractor', href: 'src/pdf/extractor/extract.html', keywords: 'export data' },
+          { label: 'Dictionary', href: 'src/miscellaneous/dictionary/index.html', keywords: 'dictionary' },    
           { label: 'MarkDown Editor', href: 'src/miscellaneous/MarkDownEditor/markdown.html', keywords: 'markdown editor' } 
         ]
       },
